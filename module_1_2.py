7944c08f5c05bb82274f55a39fcc777449db5def
Mikhail1283 <memfis1283@mail.ru>
2024-08-01 15:47:12 +0500

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,4 @@
+home_work = 12
+time_home_work = 1.5
+name_course = 'Python,'
+print(('Курс:'), (name_course), ('всего задач:'), (home_work),",", ('затрачено часов:'), (time_home_work),",", ('среднее время выполнения'), (time_home_work)/(home_work),".")
